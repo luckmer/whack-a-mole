a5eb8d7ec484202cc85bca31a1cd8b82cd3e3e5a
luckmer <woszczo04@gmail.com>
2021-05-21 17:43:54 +0200

    import

---
+++ src/ts/import.ts
@@ -0,0 +1,8 @@
+const Game = document.querySelector(".panel");
+const Life = document.querySelector("#life");
+const Points = document.querySelector("#points");
+const Time = document.querySelector("#time");
+const Buttons = document.querySelector(".buttons");
+const gameResult = document.querySelector(".text");
+
+export { Game, Life, Points, Time, Buttons, gameResult };
