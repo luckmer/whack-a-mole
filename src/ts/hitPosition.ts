07a523c4d48eb65403933e80bf5b3fc8f46f76e8
luckmer <woszczo04@gmail.com>
2021-05-21 17:43:45 +0200

    hit position

---
+++ src/ts/hitPosition.ts
@@ -0,0 +1,6 @@
+export class GlobalVars {
+  public static hitPosition: string | null = "";
+  public static speed: number = 2000;
+  public static disable: any = undefined;
+  public static PlayTimer: any = null;
+}
